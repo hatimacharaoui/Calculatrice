dd6c9fd33917ab11f38dfe70a6c467e925812f99
hatimacharaoui <152444218+hatimacharaoui@users.noreply.github.com>
2025-09-28 22:26:21 +0100

    Delete Challenge6.js
    Delete

--- Challenge6.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-
-let distance = +prompt("Entrer la distance en km : ");
-let vitesse = +prompt("Entrer la vitesse de voiture en km/h: ");
-
-let temps = distance/vitesse;
-console.log("le temps de trajet : "+temps+" h");
-
