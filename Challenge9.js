6e2de3ea3f3d87834204cc8f22bbe0e6691c434e
hatimacharaoui <152444218+hatimacharaoui@users.noreply.github.com>
2025-09-28 22:26:54 +0100

    Delete Challenge9.js
    Delete

--- Challenge9.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")();
-
-
- let distance = prompt("Entrer la distance en kilomètres : ");
-
- let Mile = distance / 1.609
- console.log("la distance en miles : "+Mile);
