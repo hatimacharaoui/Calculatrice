5ae976e759277b62bf35e8d659a0776999c4bf67
hatimacharaoui <152444218+hatimacharaoui@users.noreply.github.com>
2025-09-29 12:25:13 +0100

    Update Calculatrice.js

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,37 +1,5 @@
 const prompt = require("prompt-sync")();
 
-function EntrerNombre1() {
-  let nombre1;
-  do {
-    nombre1 = +prompt("Entrer le premier nombre: ");
-  } while (nombre1 <= 0);
-  return nombre1;
-}
-function EntrerNombre2() {
-  let nombre2;
-  do {
-    nombre2 = +prompt("Entrer le deuxieme nombre: ");
-  } while (nombre2 <= 0);
-  return nombre2;
-}
-
-function MenuPrincipal() {
-  let choix = 1;
-  do {
-    console.log("*******  Choisir une opération:  *******");
-    console.log("Pour l'Addition entrer (1): ");
-    console.log("Pour la Soustraction entrer (2): ");
-    console.log("Pour la Multiplication entrer (3): ");
-    console.log("Pour la Division entrer (4): ");
-    console.log("Pour la Puissance entrer (5): ");
-    console.log("Pour le Racine carrée entrer (6): ");
-    console.log("Pour le Factorielle entrer (7): ");
-
-    choix = +prompt("Opération: ");
-  } while (choix < 1 || choix > 7);
-  return choix;
-}
-
 function Addition(nbr1, nb2) {
   let addition = nbr1 + nb2;
   return addition;
@@ -60,52 +28,104 @@ function Factorielle(nbr1) {
   let resultat = 1;
   for (let i = 1; i <= nbr1; i++) {
     resultat *= i;
+    return resultat;
   }
-  return resultat;
 }
 function calculer() {
-  let nb1 = EntrerNombre1();
-  let nbr2 = EntrerNombre2();
-  let choix = MenuPrincipal();
-  let resultat;
-  if (choix == 1) {
-    resultat = Addition(nb1, nbr2);
-    console.log(nb1 +" + "+ nbr2 + " = "+resultat);
-  } else if (choix == 2) {
-    resultat = Soustraction(nb1, nbr2);
-    console.log(nb1 +" - "+ nbr2 + " = "+resultat);
-  } else if (choix == 3) {
-    resultat = Multiplication(nb1, nbr2);
-    console.log(nb1 +" * "+ nbr2 + " = "+resultat);
-  } else if (choix == 4) {
-    resultat = Division(nb1, nbr2);
-    console.log(nb1 +" / "+ nbr2 + " = "+resultat);
-  } else if (choix == 5) {
-    resultat = Puissance(nb1, nbr2);
-    console.log(nb1 +" ^ "+ nbr2 + " = "+resultat);
-  } else if (choix == 6) {
-    resultat = RacineCarrée(nb1);
-    console.log(nb1 +" racine "+ " = "+resultat);
-  } else if (choix == 7) {
-    resultat = Factorielle(nb1);
-    console.log(nb1 +" ! " + " = "+resultat);
+  let choix;
+  let repeter;
+  let tableau = [];
+  do {
+    do {
+      console.log("*******  Choisir une opération:  *******");
+      console.log("Pour l'Addition entrer (1): ");
+      console.log("Pour la Soustraction entrer (2): ");
+      console.log("Pour la Multiplication entrer (3): ");
+      console.log("Pour la Division entrer (4): ");
+      console.log("Pour la Puissance entrer (5): ");
+      console.log("Pour le Racine carrée entrer (6): ");
+      console.log("Pour le Factorielle entrer (7): ");
+
+      choix = prompt("Opération: ");
+    } while (choix < 1 || choix > 7);
+    let nombre1;
+    let nombre2;
+    let resultat;
+    switch (choix) {
+      case "1":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+        nombre2 = +prompt("Entrer le deuxieme nombre: ");
+
+        resultat = Addition(nombre1, nombre2);
+        console.log(nombre1 + " + " + nombre2 + " = " + resultat);
+        tableau.push(nombre1 + " + " + nombre2 + " = " + resultat);
+        break;
+      case "2":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+        nombre2 = +prompt("Entrer le deuxieme nombre: ");
+
+        resultat = Soustraction(nombre1, nombre2);
+        console.log(nombre1 + " - " + nombre2 + " = " + resultat);
+        tableau.push(nombre1 + " - " + nombre2 + " = " + resultat);
+        break;
+      case "3":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+        nombre2 = +prompt("Entrer le deuxieme nombre: ");
+        resultat = Multiplication(nombre1, nombre2);
+        console.log(nombre1 + " - " + nombre2 + " = " + resultat);
+        tableau.push(nombre1 + " - " + nombre2 + " = " + resultat);
+        break;
+      case "4":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+
+        nombre2 = +prompt("Entrer le deuxieme nombre: ");
+        if (nombre2 == 0) {
+          console.log("Impossible de diviser par zéro");
+          break;
+        } else {
+          resultat = Division(nombre1, nombre2);
+          console.log(nombre1 + " / " + nombre2 + " = " + resultat);
+          tableau.push(nombre1 + " / " + nombre2 + " = " + resultat);
+        }
+        break;
+      case "5":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+        nombre2 = +prompt("Entrer le deuxieme nombre: ");
+
+        resultat = Puissance(nombre1, nombre2);
+        console.log(nombre1 + " ^ " + nombre2 + " = " + resultat);
+        tableau.push(nombre1 + " ^ " + nombre2 + " = " + resultat);
+        break;
+      case "6":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+
+        resultat = RacineCarrée(nombre1);
+        console.log(nombre1 + " Racine = " + resultat);
+        tableau.push(nombre1 + " Racine = " + resultat);
+        break;
+      case "7":
+        nombre1 = +prompt("Entrer le premier nombre: ");
+
+        resultat = Factorielle(nombre1);
+        console.log(nombre1 + " ! = " + resultat);
+        tableau.push(nombre1 + " ! = " + resultat);
+        break;
+
+      default:
+        console.log("Entrer un nombre dans la liste (1-7)");
+        break;
+    }
+    repeter = prompt(
+      "Voulez vous continuer les calcules ou Quitter? Oui/Non :"
+    );
+  } while (repeter === "oui");
+
+  let affichage = prompt(
+    "Voulez vous afficher l'historique des calculs? Oui/Non :"
+  );
+  if (affichage === "oui") {
+    console.log(tableau);
   }
-  return resultat;
 }
 
-function main(){
-let table = [];
-let repeter;
-do {
-  let calcul = calculer()
-  table.push(calcul);
-  repeter = prompt("Voulez vous continuer les calcules ou Quitter? Oui/Non :");
-} while (repeter === "oui");
-
-let affichage = prompt("Voulez vous afficher l'historique des calculs? Oui/Non :");
-if (affichage === "oui") {
-  console.log("Historique: "+table);
-}
-}
-
-main();
+calculer();
